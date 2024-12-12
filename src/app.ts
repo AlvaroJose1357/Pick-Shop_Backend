dbc680594aa62936044ad33fc0982526e65c3b79
Juan Esteban Perea Olivares <jeperea45@gmail.com>
2024-12-12 15:22:19 -0500

    feat: Backend full

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import cors from 'cors';
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./config/swaggerConfig";
 import connectBD from "./config/BD";
@@ -12,6 +13,8 @@ import userRoute from "./routes/userRoute";
 const app: Application = express();
 
 connectBD();
+
+app.use(cors());
 
 app.use("/api/cart", cartRoute);
 app.use("/api/categories", categoryProductRoute);
